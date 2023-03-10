3103beefec1d6942b4921aeba00e8389403b7bce
Alex Cutforth <alex@densem.net>
2023-03-10 16:12:25 +1300

    setup prisma and requirements

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # 13DTPProject
  
+Run `cd server ; py -m venv .venv ; .venv/Scripts/Activate.ps1 ; py -m pip install -r requirements.txt ; prisma generate ; py main.py` please
--- server/database/__init__.py
+++
@@ -1,6 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-
-engine = create_engine("sqlite:///server\database.db")
-
-db_session = sessionmaker(bind=engine)()
