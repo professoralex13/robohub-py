94a79a53db12f05b81a5125ade94facaa5df8d1e
Alex Cutforth <alex@densem.net>
2023-03-14 22:48:48 +1300

    fix: live email/username taken status concurrency

--- client/src/pages/SignUp.tsx
+++ client/src/pages/SignUp.tsx
@@ -7,25 +7,52 @@ import { ErrorBox } from '../components/Notication';
 import { getResponseErrorMessage, requestUnauthorized, useRequest } from '../hooks/useRequest';
 
 /**
- * Prevents unccesary asyncronous validation calls by rate limiting and only re executing when the value changes
+ * Prevents unccesary asyncronous validation calls by only re-xecuting when value changes and ensuring only one request is queued at a time
  * @param asyncValidate Validation callback function
- * @param maxRate Maximum rate the validation can be called in milliseconds
  * @returns The wrapped validator function
  */
-const cacheRateLimitedTest = (asyncValidate: (val: string) => Promise<boolean>, maxRate: number = 1000) => {
-    let valid = false;
-    let previousValue = '';
-    let previousTimestamp = Date.now();
+const concurrentControledTest = (asyncValidate: (value: string) => Promise<boolean>) => {
+    let valid = false; // Stores latest validation result
+    let previousValue = ''; // Stores previous input value that was validated
+    let currentPromise: Promise<boolean> | null = null; // Stores reference to currently executing validation promise
+    const requestStack: (() => void)[] = []; // stack to store requests made while waiting for the current promise to resolve
+
+    const executeRequest = async (value: any) => {
+        const response = await asyncValidate(value);
+
+        valid = response;
+        currentPromise = null;
+
+        // resolve all the requests in the stack with the validation result
+        while (requestStack.length > 0) {
+            const nextRequest = requestStack.pop();
+            if (nextRequest) {
+                nextRequest();
+            }
+        }
+
+        return response;
+    };
 
     return async (value: any) => {
-        if (value !== previousValue && Date.now() - previousTimestamp > maxRate) {
-            const response = await asyncValidate(value);
-            previousValue = value;
-            valid = response;
-            previousTimestamp = Date.now();
-            return response;
+        if (value === previousValue) { // if the value is the same as the previous one, return the cached result
+            return valid;
         }
-        return valid;
+
+        if (!currentPromise) { // if there is no current promise, make a new one
+            previousValue = value;
+            currentPromise = executeRequest(value);
+            return currentPromise;
+        }
+
+        return new Promise<boolean>((resolve) => {
+            // if there is a current promise, add the resolve function to the stack
+            // it will be called once the current promise is resolved
+            requestStack.push(async () => {
+                await currentPromise; // wait for the current promise to resolve
+                resolve(valid); // resolve with the cached validation result
+            });
+        });
     };
 };
 
@@ -36,13 +63,14 @@ const SignUpSchema = Yup.object().shape({
     email: Yup
         .string()
         .matches(/^[\w-.]+@([\w-]+\.)+[\w-]{2,4}$/, 'Not valid email')
-        .test('is-available', 'Email taken', cacheRateLimitedTest(async (value) => !(await requestUnauthorized<boolean>(`/auth/email-taken/${value}`, 'GET')).data))
+        .test('is-available', 'Email taken', concurrentControledTest(async (value) => !(await requestUnauthorized<boolean>(`/auth/email-taken/${value}`, 'GET')).data))
         .required('Required'),
     username: Yup
         .string()
         .min(2, 'Too short')
         .max(15, 'Too long')
-        .test('is-available', 'Username taken', cacheRateLimitedTest(async (value) => !(await requestUnauthorized<boolean>(`/auth/username-taken/${value}`, 'GET')).data))
+        // TODO: ensure username is valid url string before sending to prevent 404 errors, or use a post request
+        .test('is-available', 'Username taken', concurrentControledTest(async (value) => !(await requestUnauthorized<boolean>(`/auth/username-taken/${value}`, 'GET')).data))
         .required('Required'),
     password: Yup
         .string()
